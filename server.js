e033f2f69d1aa22cb87b9c05a2821390964ed8f5
Chris Kabana <chris.kabana@gmail.com>
2020-10-29 13:53:39 -0500

    setting up server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//requiring static files in public folder
+app.use(express.static("public"));
+
+//connection to mongo database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+//requiring routes
+app.use(require("./routes/api.js"));
+
+//connection to port
+app.listen(PORT, () => {
+    console.log("App running on port ${PORT}!");
+});
